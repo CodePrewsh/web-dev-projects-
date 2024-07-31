247c6f5dfda0ba74085874b55e76aa5284b93edd
precious <bhembepreciouspt@gmail.com>
2024-07-31 08:34:15 +0200

    project

---
+++ MERN-Auth/backend/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+
+const router = express.Router()
+
+//login route
+routwer.router.post('login', () => {})
+
+//signup route
+router.post('/signup', () => {})
+
+module.exports - router
